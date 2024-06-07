07a018477dc9f1e119e08ced5bf5525d62365cd2
Bernd Deichmann <bernd.deichmann@gmail.com>
2024-06-07 11:51:36 +0200

    get methods have abstract_get members set

--- src/pyprotosdc/consumer/serviceclients/getservice.py
+++ src/pyprotosdc/consumer/serviceclients/getservice.py
@@ -4,7 +4,11 @@ import uuid
 from typing import TYPE_CHECKING
 from dataclasses import dataclass
 from org.somda.protosdc.proto.model import sdc_services_pb2_grpc, sdc_messages_pb2
-
+from org.somda.protosdc.proto.model.biceps.abstractget_pb2 import AbstractGetMsg
+from org.somda.protosdc.proto.model.biceps.getmdib_pb2 import GetMdibMsg
+from org.somda.protosdc.proto.model.biceps.getmddescription_pb2 import GetMdDescriptionMsg
+from org.somda.protosdc.proto.model.biceps.getmdstate_pb2 import GetMdStateMsg
+from org.somda.protosdc.proto.model.biceps.getcontextstates_pb2 import GetContextStatesMsg
 from sdc11073.mdib.mdibbase import MdibVersionGroup
 
 from pyprotosdc.mapping.msgtypes_mappers import get_mdib_version_group
@@ -53,7 +57,11 @@ class GetServiceWrapper():
         request = sdc_messages_pb2.GetMdibRequest()
         request.addressing.message_id = uuid.uuid4().urn
         request.addressing.action = GetAction.GetMdibRequest
-
+        # make child fields payload and payload.abstract_get available
+        payload = GetMdibMsg()
+        abstract_get = AbstractGetMsg()
+        payload.abstract_get.MergeFrom(abstract_get)
+        request.payload.MergeFrom(payload)
         response = self._stub.GetMdib(request)
 
         mdib_version_group_msg = get_p_attr(response.payload.abstract_get_response, 'MdibVersionGroup')
@@ -61,10 +69,19 @@ class GetServiceWrapper():
         descriptors, states = self._msg_reader.read_get_mdib_response(response)
         return GetMdibResponseData(mdib_version_group, response, descriptors, states)
 
-    def get_md_description(self) -> GetMdDescriptionResponseData:
+    def get_md_description(self, handles: list[str] | None = None) -> GetMdDescriptionResponseData:
         request = sdc_messages_pb2.GetMdDescriptionRequest()
         request.addressing.message_id = uuid.uuid4().urn
         request.addressing.action = GetAction.GetMdDescriptionRequest
+
+        # make child fields payload and payload.abstract_get available
+        payload = GetMdDescriptionMsg()
+        if handles:
+            for handle in handles:
+                payload.handle_ref.add().string = handle
+        abstract_get = AbstractGetMsg()
+        payload.abstract_get.MergeFrom(abstract_get)
+        request.payload.MergeFrom(payload)
 
         response = self._stub.GetMdDescription(request)
 
@@ -73,10 +90,21 @@ class GetServiceWrapper():
         descriptors = self._msg_reader.read_md_description(response.payload.md_description)
         return GetMdDescriptionResponseData(mdib_version_group, response, descriptors)
 
-    def get_md_state(self) -> GetMdStateResponseData:
+    def get_md_state(self, handles: list[str] | None = None) -> GetMdStateResponseData:
         request = sdc_messages_pb2.GetMdStateRequest()
         request.addressing.message_id = uuid.uuid4().urn
         request.addressing.action = GetAction.GetMdStateRequest
+
+        # make child fields payload and payload.abstract_get available
+        payload = GetMdStateMsg()
+
+        abstract_get = AbstractGetMsg()
+        if handles:
+            for handle in handles:
+                payload.handle_ref.add().string = handle
+
+        payload.abstract_get.MergeFrom(abstract_get)
+        request.payload.MergeFrom(payload)
 
         response = self._stub.GetMdState(request)
         response.addressing.message_id = uuid.uuid4().urn
@@ -87,10 +115,20 @@ class GetServiceWrapper():
         states = self._msg_reader.read_states(response.payload.md_state.state, mdib)
         return GetMdStateResponseData(mdib_version_group, response, states)
 
-    def get_context_states(self) -> GetContextStatesResponseData:
+    def get_context_states(self, handles: list[str] | None = None) -> GetContextStatesResponseData:
         request = sdc_messages_pb2.GetContextStatesRequest()
         request.addressing.message_id = uuid.uuid4().urn
         request.addressing.action = GetAction.GetContextStateRequest
+
+        # make child fields payload and payload.abstract_get available
+        payload = GetContextStatesMsg()
+        if handles:
+            for handle in handles:
+                payload.handle_ref.add().string = handle
+
+        abstract_get = AbstractGetMsg()
+        payload.abstract_get.MergeFrom(abstract_get)
+        request.payload.MergeFrom(payload)
 
         response = self._stub.GetContextStates(request)
         mdib_version_group_msg = get_p_attr(response.payload.abstract_get_response, 'MdibVersionGroup')
